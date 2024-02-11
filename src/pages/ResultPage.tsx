e0b36d13379f6a3a328358d2ef7c8f05619030f6
Anshuman <youwillknow360@gmail.com>
2024-02-11 12:46:05 -0800

    fixes UI

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -2,10 +2,9 @@ const ResultPage = () => {
   return (
     <>
     <nav className="navBar">Crushed</nav>
-    <div style = {{height:'92vh', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+    <div style = {{height:'92vh', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', textAlign:'center'}}>
       
-      <h1>Result will be declared at</h1>
-      <h1>13th midnight</h1>
+      <h1>Result will be declared at 13th midnight</h1>
     </div>
     </>
   )
