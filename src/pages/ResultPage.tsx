809400f79dfd4b78c4bd4fb9613c65dfedc22f94
Rashmi <rpsatapathy04@gmail.com>
2024-02-14 00:13:26 +0530

    result card update

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -1,13 +1,27 @@
 import NavBar from "./Components/NavBar"
+import Matchcard from "./Matchcard";
+import { CiSearch } from "react-icons/ci";
+
+
 
 const ResultPage = () => {
   return (
     <>
     <NavBar/>
-    <div style = {{height:'92vh', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', textAlign:'center'}}>
-      
-      <h1>Matches will be revealed on 13th midnight</h1>
+    <div className="resultPage">
+    <div className="resultPageSearch cnt" >
+    <p>Male Responses- 490 </p>  
+    <p>Female Responses- 70 </p>      
     </div>
+
+    <div className="resultPageSearch">
+     <input type="text" className="search" placeholder="Enter Name..." />
+     <button className="resultsearchbtn"><CiSearch /></button>
+    </div>
+    
+    <Matchcard/>
+   </div>
+
     </>
   )
 }
